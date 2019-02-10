da7a1ad93db1f8beaf724ab7c2acb81ee3560a91
Adam Gordon <adamjgordon24@gmail.com>
2019-02-10 02:01:38 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,7 +15,7 @@ def load_library(file_path)
 end
 
 def get_japanese_emoticon(file_path, emoticon)
-  # code goes here
+  full_libarary = load_library(file_path)
 end
 
 def get_english_meaning(file_path, emoticon)
