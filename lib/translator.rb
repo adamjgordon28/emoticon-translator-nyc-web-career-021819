aafff95b4f1aa4c5c02ec166a0f7e84549e5a8fe
Adam Gordon <adamjgordon24@gmail.com>
2019-02-10 02:03:56 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -20,6 +20,7 @@ def get_japanese_emoticon(file_path, emoticon)
     return full_library["get_emoticon"][emoticon]
   else 
     return "Sorry, that emoticon was not found"
+  end
 end
 
 def get_english_meaning(file_path, emoticon)
